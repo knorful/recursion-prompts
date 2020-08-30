08afb3e969001e2d941231d76e46f49ed41bf293
Kris Norful <krisnorful@gmail.com>
2020-08-30 18:22:52 -0500

    Completed Sum Integers In Array

--- src/recursion.js
+++ src/recursion.js
@@ -21,6 +21,17 @@ var factorial = function (n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function (array) {
+  let copyArr = array.slice();
+
+  if (copyArr.length === 0) {
+    return 0;
+  }
+
+  if (copyArr.length === 1) {
+    return copyArr[0];
+  } else {
+    return copyArr.pop() + sum(copyArr);
+  }
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
