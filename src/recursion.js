9187e460a43755133707259ef711bd3a0f6e3e69
Kris Norful <krisnorful@gmail.com>
2020-08-31 13:26:20 -0500

    Completed Integer Range

--- src/recursion.js
+++ src/recursion.js
@@ -87,14 +87,22 @@ var sumBelow = function (n) {
   return n += sumBelow(n);
 };
 
-
-console.log(sumBelow(5));
-
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function (x, y) {
+  var rangeArr = [];
+  //this determines if your range goes up or down
+  const step = x > y ? -1 : 1;
+  if (x === y) {
+    return [];
+  }
+  if (x === y - step) {
+    return rangeArr;
+  }
+  return rangeArr.concat(x + step, range(x + step, y));
 };
 
+range(7, 2);
 // 7. Compute the exponent of a number.
 // The exponent of a number says how many times the base number is used as a factor.
 // 8^2 = 8 x 8 = 64. Here, 8 is the base and 2 is the exponent.
